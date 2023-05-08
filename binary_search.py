466fade55c716529aa639df46851edad9c1f1b9c
庭庭 <qsid1130@gmail.com>
2023-05-08 18:17:51 +0800

    fixed one error

--- binary_search.py
+++ binary_search.py
@@ -1,33 +1,3 @@
-# def binary_search(A, number):
-#     first = 0
-#     last = len(A) - 1
-#     while first < last:
-#         mid = (first + last) // 2
-#         if number < A[mid]:
-#             last = mid - 1
-#
-#         elif number > A[mid]:
-#             first = mid + 1
-#
-#         else:
-#             return mid
-#     return mid
-#
-#
-# def binary_search_recursive(arr, number, first, last):
-#     if first >= last:
-#         return -1
-#     mid = (first + last) // 2
-#     if number > arr[mid]:
-#         first = mid + 1
-#         return binary_search_recursive(arr, number, first, last)
-#     elif number < arr[mid]:
-#         last = mid - 1
-#         return binary_search_recursive(arr, number, first, last)
-#     else:
-#         return mid
-
-
 def binary_search(arr, number):
     first = 0
     last = len(arr) - 1
@@ -36,7 +6,7 @@ def binary_search(arr, number):
         if arr[mid] < number:
             first = mid + 1
         elif arr[mid] > number:
-            last = mid + 1
+            last = mid - 1
         elif arr[mid] == number:
             return mid
         else:
@@ -59,7 +29,6 @@ def binary_search_recursive(arr, number, first, last):
         elif arr[mid] == number:
             return mid
     return mid
-
 
 
 if __name__ == '__main__':
